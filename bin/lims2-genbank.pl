3b12713467a375c8689861017073b733e01ee854
Sajith Perera <sajith.perera@gmail.com>
2014-04-17 15:21:07 +0100

    can specify just a plate name, will download all the genbank files for the well on that plate into a folder named after the plate, also add a option to specify a stage

--- bin/lims2-genbank.pl
+++ bin/lims2-genbank.pl
@@ -8,53 +8,76 @@ use LIMS2::Model;
 use Bio::SeqIO;
 use LIMS2::Model::Util::EngSeqParams qw( generate_well_eng_seq_params );
 use Log::Log4perl ':easy';
+use Path::Class qw( dir );
 
-{
+my $log_level = $WARN;
+GetOptions(
+    'help'          => sub { pod2usage( -verbose => 1 ) },
+    'man'           => sub { pod2usage( -verbose => 2 ) },
+    'debug'         => sub { $log_level = $DEBUG },
+    'verbose'       => sub { $log_level = $INFO },
+    'plate_name=s'  => \my $plate_name,
+    'well_name=s'   => \my $well_name,
+    'well_id=s'     => \my $well_id,
+    'cassette=s'    => \my $cassette,
+    'backbone=s'    => \my $backbone,
+    'recombinase=s' => \my @recombinases,
+    'stage=s'       => \my $stage,
+    'config=s'      => \my $config,
+    'format=s'      => \my $format,
+) or pod2usage(2);
+Log::Log4perl->easy_init( { level => $log_level, layout => '%p %m%n' } );
 
-    my $log_level = $WARN;
-    GetOptions(
-        'help'          => sub { pod2usage( -verbose    => 1 ) },
-        'man'           => sub { pod2usage( -verbose    => 2 ) },
-        'debug'         => sub { $log_level = $DEBUG },
-        'verbose'       => sub { $log_level = $INFO },
-        'plate_name=s'  => \my $plate_name,
-        'well_name=s'   => \my $well_name,
-        'well_id=s'     => \my $well_id,
-        'cassette=s'    => \my $cassette,
-        'backbone=s'    => \my $backbone,
-        'recombinase=s' => \my @recombinases,
-        'config=s'      => \my $config,
-        'format=s'      => \my $format,
-    ) or pod2usage(2);
-    Log::Log4perl->easy_init( { level => $log_level, layout => '%p %m%n' } );
+my $model = LIMS2::Model->new( user => 'lims2' );
 
-    my $model = LIMS2::Model->new( user => 'lims2' );
+my $eng_seq_builder = $config ? EngSeqBuilder->new( configfile => $config )
+                              : EngSeqBuilder->new;
 
-    my $eng_seq_builder = $config ? EngSeqBuilder->new( configfile => $config )
-                                  : EngSeqBuilder->new;
+my %params;
+$params{cassette}    = $cassette if $cassette;
+$params{backbone}    = $backbone if $backbone;
+$params{stage}       = $stage if $stage;
+$params{recombinase} = \@recombinases if @recombinases;
 
-    my %params;
-    if ( $well_id ) {
-        $params{well_id} = $well_id;
-    }
-    elsif( $plate_name && $well_name ) {
-        $params{plate_name} = $plate_name;
-        $params{well_name} = $well_name;
-    }
-    else {
-        die( 'Need well_id, or plate_name and well_name' );
-    }
+if ( $well_id ) {
+    $params{well_id} = $well_id;
+    genbank_for_well( \%params, \*STDOUT );
+}
+elsif( $plate_name && $well_name ) {
+    $params{plate_name} = $plate_name;
+    $params{well_name} = $well_name;
+    genbank_for_well( \%params, \*STDOUT );
+}
+elsif ( $plate_name ) {
+    genbank_for_plate( $plate_name, \%params );
+}
+else {
+    die( 'Need well_id, or plate_name specified' );
+}
 
-    $params{cassette} = $cassette if $cassette;
-    $params{backbone} = $backbone if $backbone;
-    $params{recombinase} = \@recombinases if @recombinases;
+sub genbank_for_well {
+    my ( $params, $fh ) = @_;
 
-    my ( $method, $well, $eng_seq_params ) = generate_well_eng_seq_params( $model, \%params );
+    my ( $method, $well, $eng_seq_params ) = generate_well_eng_seq_params( $model, $params );
 
-	my $seq = $eng_seq_builder->$method( %{ $eng_seq_params } );
+    my $seq = $eng_seq_builder->$method( %{ $eng_seq_params } );
 
-    my $seq_io = Bio::SeqIO->new( -fh => \*STDOUT, -format => $format || 'genbank' );
+    my $seq_io = Bio::SeqIO->new( -fh => $fh, -format => $format || 'genbank' );
     $seq_io->write_seq( $seq );
+}
+
+sub genbank_for_plate {
+    my ( $plate_name, $params ) = @_;
+    my $dir = dir( $plate_name );
+    $dir->mkpath;
+
+    my $plate = $model->retrieve_plate( { name => $plate_name } );
+
+    for my $well ( $plate->wells ) {
+        my $file = $dir->file( $well->as_string . '.gbk' );
+        $params->{well_id} = $well->id;
+        genbank_for_well( $params, $file->openw );
+    }
 }
 
 __END__
@@ -77,6 +100,7 @@ lims2-genbank.pl - generate genbank file for lims2 well
       --cassette        Specify / override cassette value
       --backbone        Specify / override backbone value
       --recombinase     Specify / override recombinase ( can specify multiple times )
+      --stage           Stage is either allele or vector
       --config          Custom config for EngSeqBuilder
       --format          Format of sequence output, default Genbank
 
